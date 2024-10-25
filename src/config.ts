4b1289a53395dcf3a119fd3dd6c6f5a66ceba3de
Dylan Burkey <dylan.burkey@zebraskimmers.com>
2024-10-24 22:58:10 -0400

    [short summary of changes] Small changes
    [Describe previous functionality and its issues]
    [Describe current functionality]
    [Describe reasoning behind the chosen solution]

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const SITE: Site = {
   profile: "https://dylanburkey.box",
   desc: "Web3 and Web2 development and marketing are intersecting. Understanding the intersection is key to success.",
   title: "Dylan Burkey",
-  ogImage: "dylan-burkey.jpg",
+  ogImage: "(@assets/images/images/index-og.webp",
   lightAndDarkMode: true,
   postPerIndex: 6,
   postPerPage: 6,
