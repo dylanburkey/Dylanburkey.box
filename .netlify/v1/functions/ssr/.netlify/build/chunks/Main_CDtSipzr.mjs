d2af121a3b0b7fcc7908d6a6f6b778cb0f31070b
Dylan Burkey <dylanburkey@outlook.com>
2025-01-29 13:16:24 -0500

    add files

---
+++ .netlify/v1/functions/ssr/.netlify/build/chunks/Main_CDtSipzr.mjs
@@ -0,0 +1,15 @@
+import { b as createAstro, c as createComponent, r as renderTemplate, a as renderComponent, m as maybeRenderHead, e as addAttribute, f as renderTransition, d as renderSlot } from './astro/server_D50-pur6.mjs';
+import 'kleur/colors';
+import { $ as $$Breadcrumbs } from './Breadcrumbs_CpblT99Q.mjs';
+/* empty css                          */
+/* empty css                          */
+
+const $$Astro = createAstro("https://dylanburkey.com");
+const $$Main = createComponent(($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Main;
+  const { props } = Astro2;
+  return renderTemplate`${renderComponent($$result, "Breadcrumbs", $$Breadcrumbs, { "data-astro-cid-hsp6otuf": true })} ${maybeRenderHead()}<main id="main-content" data-astro-cid-hsp6otuf> ${"titleTransition" in props ? renderTemplate`<h1 data-astro-cid-hsp6otuf> ${props.pageTitle[0]} <span data-astro-cid-hsp6otuf${addAttribute(renderTransition($$result, "hn2qarie", "", props.titleTransition), "data-astro-transition-scope")}> ${props.pageTitle[1]} </span> </h1>` : renderTemplate`<h1 class="roboto-bold" data-astro-cid-hsp6otuf>${props.pageTitle}</h1>`} <p data-astro-cid-hsp6otuf>${props.pageDesc}</p> ${renderSlot($$result, $$slots["default"])} </main> `;
+}, "/Users/dylanburkey/Documents/dev/Dylanburkey.box/src/layouts/Main.astro", "self");
+
+export { $$Main as $ };
